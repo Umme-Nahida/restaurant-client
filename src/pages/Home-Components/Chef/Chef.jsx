513d256b5775bed5a9f3a50d555b3afc27293199
Umme-Nahida <mysuccessmission@gmail.com>
2024-08-12 21:39:39 +0600

    some chages

---
+++ src/pages/Home-Components/Chef/Chef.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import Cart from "../../../shares/Cart/Cart";
+
+const Chef = () => {
+   const [menus,setMenus] = useState([]);
+
+    useEffect(()=>{
+        fetch("menu.json")
+        .then(res=>res.json())
+        .then(data=>{
+          setMenus(data)
+        })
+    },[])
+    console.log(menus)
+
+    return (
+        <div className="py-5 lg:py-20">
+            <SectionTitle
+            subTitle={"---Should Try---"}
+            heading={"CHEF RECOMMENDS"}
+            ></SectionTitle>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-5 lg:gap-5 justify-items-center px-5 md:px-20">
+                {
+                    menus.slice(0,3).map(item=>(
+                        <Cart
+                        key={item._id}
+                        item={item}
+                        ></Cart>
+                    ))
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Chef;
