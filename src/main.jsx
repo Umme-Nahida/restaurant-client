4d5d7fcde5c31493a84bf3d5846e295d5f01e30d
Umme-Nahida <mysuccessmission@gmail.com>
2024-08-13 15:37:41 +0600

    add react helmet to all page

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import {
-  RouterProvider,
-} from "react-router-dom";
-import './index.css'
-import router from './Route/router';
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import router from "./Route/router";
+import { HelmetProvider } from "react-helmet-async";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <div className=''>
-   <RouterProvider router={router} />
-   </div>
-</React.StrictMode>
-)
+    <HelmetProvider>
+      <div className="">
+        <RouterProvider router={router} />
+      </div>
+    </HelmetProvider>
+  </React.StrictMode>
+);
--- src/pages/OurMenu/OurMenu.jsx
+++ src/pages/OurMenu/OurMenu.jsx
@@ -1,10 +1,18 @@
+import { Helmet } from "react-helmet-async";
 import img from "../../assets/menu/banner3.jpg";
 import Cover from "../../shares/Cover/Cover";
+import PopularMenu from "../Home-Components/PopularMenu/PopularMenu";
 
 const OurMenu = () => {
   return (
    <div>
-     <Cover img={img} title={"OUR MENU"} text={"WOULD YOU LIKE TO TRY A DISH?"}></Cover>
+    <Helmet>
+        <title>Bistro Boss | OurMenu</title>
+      </Helmet>
+     <Cover 
+     img={img}
+     title={"OUR MENU"} text={"WOULD YOU LIKE TO TRY A DISH?"}></Cover>
+     <PopularMenu></PopularMenu>
    </div>
   );
 };
