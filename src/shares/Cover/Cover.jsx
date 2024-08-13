df1476a07e32f3cc549db1c1e2bafa6697885e62
Umme-Nahida <mysuccessmission@gmail.com>
2024-08-13 15:36:38 +0600

    added cover section in ourmenu page

--- src/shares/Cover/Cover.jsx
+++ src/shares/Cover/Cover.jsx
@@ -1,9 +1,19 @@
+
 /* eslint-disable react/prop-types */
 
+import { Parallax } from "react-parallax";
+
 const Cover = ({img,title,text}) =>{
+
     return (
+        <Parallax
+        blur={{ min: -50, max:50 }}
+        bgImage={img}
+        bgImageAlt="image"
+        strength={-200}
+    >
         <div
-        style={{ backgroundImage: `url(${img})` }}
+        style={{backgroundImage:`url(${img})`}}
         className="hero bg-base-200 h-[600px]">
         <div className="bg-black opacity-70 w-3/5 py-20 text-white">
           <div className="hero-content text-center ">
@@ -14,6 +24,8 @@ const Cover = ({img,title,text}) =>{
           </div>
         </div>
       </div>
+    </Parallax>
+       
     );
 };
 
