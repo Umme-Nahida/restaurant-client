84276e859d769ef5a938f817c03c08c0ba5dafb7
Umme-Nahida <mysuccessmission@gmail.com>
2024-08-16 00:17:22 +0600

    show add to cart count using refetch

--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,23 @@ import router from "./Route/router";
 import { HelmetProvider } from "react-helmet-async";
 import AuthProvider from "./Authentication/AuthProvider";
 import { Toaster } from "react-hot-toast";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-    <HelmetProvider>
-      <div className="">
-        <RouterProvider router={router} />
-      </div>
-      <Toaster/>
-    </HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <div className="">
+            <RouterProvider router={router} />
+          </div>
+          <Toaster />
+        </HelmetProvider>
+      </QueryClientProvider>
     </AuthProvider>
-    
   </React.StrictMode>
 );
