3e643aa2fa695de38fcfb1822c7fdab7bfe62344
Umme-Nahida <mysuccessmission@gmail.com>
2024-09-17 20:40:58 +0600

    set confirm card payment

--- src/Dashboard/Payment/Payment.jsx
+++ src/Dashboard/Payment/Payment.jsx
@@ -4,13 +4,13 @@ import '@stripe/stripe-js';
 import { loadStripe } from "@stripe/stripe-js";
 import CheckOutForm from "./CheckOutForm";
 
-const stripePromise = loadStripe('');
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GETWAY_PK);
 
 const Payment = () => {
-    const options = {
-        // passing the client secret obtained from the server
-        clientSecret: '{{CLIENT_SECRET}}',
-      };
+    // const options = {
+    //     // passing the client secret obtained from the server
+    //     clientSecret: '{{sk_test_51OZHgME5jTTsl0NgK4jroDpZyHxSvMHxzDsdVDozHIIPyHAwHRSKY2f8Xb1dtWSY1zy5lGZ1UhbPNhod2CYbJURE003eOg8yhY}}',
+    //   };
 
     return (
         <div>
