4c70647d0faf19c207e799f954691e581997dcc2
Umme-Nahida <mysuccessmission@gmail.com>
2024-10-27 20:39:41 +0600

    user navber

--- src/shares/Navber.jsx
+++ src/shares/Navber.jsx
@@ -96,12 +96,11 @@ const Navber = () => {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-base-100 text-black rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
             <li>
               <a className="justify-between">
                 Profile
-                <span className="badge">New</span>
               </a>
             </li>
             <li>
