cde4469f03d1892b16b2b2cf71726dd9febb21de
Developer Nahida <ummenahida20@gmail.com>
2025-01-09 21:47:36 +0600

    create reservation router

---
+++ src/Dashboard/Reservation/Reservation.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SectionTitle from '../../components/SectionTitle/SectionTitle';
+
+const Reservation = () => {
+    return (
+        <div>
+            <SectionTitle heading={'BOOK A TABLE'} subTitle={'Reservation'}></SectionTitle>
+
+            
+        </div>
+    );
+};
+
+export default Reservation;
