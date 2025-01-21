fa8e222a9336b4a72a9762a907532a8df28e4207
Developer Nahida <ummenahida20@gmail.com>
2025-01-21 20:20:39 +0600

    create section title page responsive for all device

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -4,7 +4,7 @@ const SectionTitle = ({subTitle,heading}) => {
     return (
         <div className="max-w-80 lg:max-w-10 md:max-w-96 mx-auto text-center my-10">
             <h1 className="text-yellow-400 mb-2">--{subTitle}--</h1>
-            <h1 className="text-xl md:text-2xl uppercase border-y-2 py-4">{heading}</h1>
+            <h1 className="text-base md:text-2xl lg:text-2xl uppercase border-y-2 py-4">{heading}</h1>
         </div>
     );
 };
